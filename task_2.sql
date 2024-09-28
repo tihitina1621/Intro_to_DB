35ee2f1d31e70c32215c712d73e1eb9cbf3a6374
tihitina1621 <titawondim@gmail.com>
2024-09-28 11:21:49 +0300

    fd

--- task_2.sql
+++ task_2.sql
@@ -1,2 +1,3 @@
 CREATE TABLE ", "Authors", "author_id ", "author_name
 Books", "title", "price", "PRIMARY KEY"]
+Customers", "customer_id ", "customer_name VARCHAR(215)", "email VARCHAR(215)", "address TEXT"]
