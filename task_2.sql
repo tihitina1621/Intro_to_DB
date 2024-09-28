4863cb3a94e80f85481c64f6157ad52bdf90f6dd
tihitina1621 <titawondim@gmail.com>
2024-09-28 11:23:55 +0300

    fdf

--- task_2.sql
+++ task_2.sql
@@ -2,3 +2,4 @@ CREATE TABLE ", "Authors", "author_id ", "author_name
 Books", "title", "price", "PRIMARY KEY"]
 Customers", "customer_id ", "customer_name VARCHAR(215)", "email VARCHAR(215)", "address TEXT"]
 Orders", "order_id INT", "customer_id INT", "order_date DATE", "FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+Order_Details", "book_id INT", "quantity DOUBLE", "FOREIGN KEY (order_id) REFERENCES Orders(order_id)", "FOREIGN KEY (book_id) REFERENCES Books(book_id)"
