74171277a92b7211e233aa6474001b54ce334263
tihitina1621 <titawondim@gmail.com>
2024-09-28 11:23:14 +0300

    fd

--- task_2.sql
+++ task_2.sql
@@ -1,3 +1,4 @@
 CREATE TABLE ", "Authors", "author_id ", "author_name
 Books", "title", "price", "PRIMARY KEY"]
 Customers", "customer_id ", "customer_name VARCHAR(215)", "email VARCHAR(215)", "address TEXT"]
+Orders", "order_id INT", "customer_id INT", "order_date DATE", "FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
